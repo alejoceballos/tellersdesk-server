16f0c89b04c724b562a2060a45f147bcd702d515
Alejo Ceballos <alejoceballos75@gmail.com>
2018-08-21 22:52:56 -0300

    Applying test configuration to prevent ESLint from checking more than it should

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 /* GET home page. */
-router.get('*', function (req, res, next) {
+router.get('*', function (req, res) {
     res.sendFile('index.html', { root: path.join(__dirname, '../public') });
 });
 
